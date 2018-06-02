499a4a7d71328b2f1a6b20e96d5de76c9591a5a1
Nathan Glenn <nathan-gary.glenn@trivago.com>
2018-06-02 22:22:47 +0200

    Simplify variable naming and syntax

--- BodyTextExtractor.py
+++ BodyTextExtractor.py
@@ -54,93 +54,76 @@ class HtmlBodyTextExtractor(HtmlTokenParser):
         HtmlTokenParser.__init__(self)
         self.encoded = [0]
         self.total_tokens_before = [0]
-        self.lookup0N = [0]
-        self.lookupN0 = [0]
+        self.num_tags_until = [0]
+        self.num_tags_after = [0]
         self.body_txt = ""
 
     def close(self):
         HtmlTokenParser.close(self)
-        self._encode_binary_tokens()
-        self._initialise_lookups()
+        self._count_cumulative_tokens()
+        self._count_cumulative_tags()
 
-    def _encode_binary_tokens(self):
+    def _count_cumulative_tokens(self):
+        """Sets the following variables in self:
+        encoded: a list of numbers indicating the number of successive tokens of a single type;
+            positive values will indicate tags, negative will indicate data.
+        total_tokens_before: indicates the total number of tokens before an entry in encoded.
+        """
         i = 0
         for token in self.binary_tokens:
+            # find boundaries between strings of tags and strings of data
             if abs(token + self.encoded[i]) < abs(self.encoded[i]):
                 self.encoded.append(0)
                 self.total_tokens_before.append(self.total_tokens_before[-1])
                 i += 1
-            self.encoded[i] = self.encoded[i] + token
-            self.total_tokens_before[i] = self.total_tokens_before[i] + 1
+            self.encoded[i] += token
+            self.total_tokens_before[i] += 1
         # total_tokens_before works better in the rest of the class if we shift all values up one index
         self.total_tokens_before.insert(0, 0)
 
-    def _initialise_lookups(self):
+    def _count_cumulative_tags(self):
+        """Sets the following variables in self:
+        num_tags_until: indicates at each position how many data tokens were seen up to that point,
+            starting at the beginning of the document.
+        num_tags_after: same as num_tags_until, but counts the tags from that point until the
+            end of the document.
+        """
         t = 0
         for token in self.encoded:
             if token > 0:
-                t = t + token
-            self.lookup0N.append(t)
-        self.encoded.reverse()
+                t += token
+            self.num_tags_until.append(t)
         t = 0
-        for token in self.encoded:
+        for token in reversed(self.encoded):
             if token > 0:
-                t = t + token
-            self.lookupN0.append(t)
-        self.encoded.reverse()
-        self.lookupN0.reverse()
-        del (self.lookupN0[0])  # will never need these values
-        del (self.lookup0N[-1])
-
-    '''
-    This method has been modified to be in O(1).
-    This version of the method works with the assumption that all nodes are
-    either text or tags. Since we can quickly find out the number of tags
-    that have occured upto a given region, and the number of total tags up
-    to that region, we can quickly calculate the number of text nodes that
-    have occured upto that region.
-
-    The original method is available as _objective_fcn_old
-    '''
+                t += token
+            self.num_tags_after.append(t)
+        self.num_tags_after.reverse()
+        # will never need these values
+        del (self.num_tags_after[0])
+        del (self.num_tags_until[-1])
 
     def _objective_fcn(self, i, j):
-        tags_to_i = self.lookup0N[i]
-        tags_after_j = self.lookupN0[j]
+        """Returns the number of tags that have been excluded plus the number of words that have been included"""
+        tags_after_j = self.num_tags_after[j]
 
-        text_to_i = self.total_tokens_before[i] - tags_to_i
-        text_to_j = self.total_tokens_before[j] - self.lookup0N[j]
+        text_to_i = self.total_tokens_before[i] - self.num_tags_until[i]
+        text_to_j = self.total_tokens_before[j] - self.num_tags_until[j]
 
         text_between_i_j = text_to_j - text_to_i
-        return_val = tags_to_i + tags_after_j + text_between_i_j
-        return return_val
-
-    '''
-    The original method, which is in O(n)
-    '''
-
-    def _objective_fcn_old(self, i, j):
-        return_val = self.lookup0N[i] + self.lookupN0[j]
-        for token in self.encoded[i:j]:
-            if token < 0:
-                return_val = return_val - token
+        return_val = self.num_tags_until[i] + tags_after_j + text_between_i_j
         return return_val
 
     @staticmethod
     def _is_tag(s):
-        if s[0] == '<' and s[-1] == '>':
-            return 1
-        else:
-            return 0
-
-    '''
-    Method which uses the modified version of _objective_fcn, this function is in O(n^2)
-    This method has also been modified to improve the finding of the 'start' and 'end' variables
-    Finally, body_text now uses the join method for building the output string
-    '''
+        return s[0] == '<' and s[-1] == '>'
 
     def body_text(self):
-        self.body_txt = ""
-        obj_max = 0
+        """Finds the longest text section which excludes the most tags using an O(N^2) exhaustive search of all
+        eligible text spans."""
+        if self.body_txt:
+            return self.body_txt
+        score_max = 0
         i_max = self.body_start_index
         j_max = len(self.encoded) - 1
         for i in range(self.body_start_index, len(self.encoded) - 1):
@@ -149,9 +132,9 @@ class HtmlBodyTextExtractor(HtmlTokenParser):
             for j in range(i, len(self.encoded)):
                 if self.encoded[j] > 0:
                     continue
-                obj = self._objective_fcn(i, j)
-                if obj > obj_max:
-                    obj_max = obj
+                score = self._objective_fcn(i, j)
+                if score > score_max:
+                    score_max = score
                     i_max = i
                     j_max = j
         start = self.total_tokens_before[i_max]
@@ -160,11 +143,6 @@ class HtmlBodyTextExtractor(HtmlTokenParser):
         self.body_txt = " ".join(txt for txt in self.tokens[start:end] if not self._is_tag(txt))
 
         return self.body_txt
-
-    def summary(self, start=0, length=255):
-        if not self.body_txt:
-            self.body_text()
-        return self.body_txt[start:(start + length)]
 
     def full_text(self):
         ft = " ".join(txt for txt in self.tokens if not self._is_tag(txt))
