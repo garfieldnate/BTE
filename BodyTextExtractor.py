b4d16dd9015dba66c37b28c716c1e55512620bdf
Nathan Glenn <nathan-gary.glenn@trivago.com>
2018-06-03 15:03:24 +0200

    Fix whitespace in output document
    Preserve linebreaks and single spaces, but strip lines and remove repeated
    spaces.

--- BodyTextExtractor.py
+++ BodyTextExtractor.py
@@ -2,20 +2,21 @@
 from __future__ import print_function
 
 import logging
+import re
 import sys
 
 from bs4 import BeautifulSoup
 
 logger = logging.getLogger(__name__)
 
-if sys.version_info >= (3, 0):
-    py2 = False
-else:
+if sys.version_info < (3, 0):
     py2 = True
+else:
+    py2 = False
 
 if py2:
     from HTMLParser import HTMLParser
-    # see https://stackoverflow.com/a/21190382/474819
+    # Fixes unicode problems; see https://stackoverflow.com/a/21190382/474819
     reload(sys)
     sys.setdefaultencoding('utf8')
 else:
@@ -23,6 +24,10 @@ else:
 
 TAG_TOKEN = 1
 DATA_TOKEN = -1
+LINEBREAK_ELEMENTS = {'address', 'article', 'aside', 'blockquote', 'canvas', 'dd', 'div', 'dl', 'dt', 'fieldset',
+                      'figcaption', 'figure', 'footer', 'form', 'h1', 'h6', 'header', 'hr', 'li', 'main', 'nav',
+                      'noscript', 'ol', 'output', 'p', 'pre', 'section', 'table', 'tfoot', 'ul', 'video', 'br'}
+MULTI_SPACES_REGEX = re.compile("[ ]+")
 
 
 class HtmlTokenParser(HTMLParser):
@@ -35,11 +40,18 @@ class HtmlTokenParser(HTMLParser):
         self.tokens = []
         self.binary_tokens = []
         self.body_start_index = 0
+        self.break_next = False
         HTMLParser.__init__(self)
 
     def handle_data(self, data):
-        # TODO: will not work for non-space languages
-        for t in data.split():
+        # TODO: will not work for non-space languages; perhaps store a negative number indicating the number of characters?
+        tokens = data.split()
+        # add back line breaks to preserve some of the document structure
+        if self.break_next:
+            if len(tokens):
+                tokens[0] = "\n" + tokens[0]
+            self.break_next = False
+        for t in tokens:
             self.tokens.append(t)
             self.binary_tokens.append(-1)
 
@@ -51,6 +63,8 @@ class HtmlTokenParser(HTMLParser):
 
     def handle_endtag(self, tag):
         self.tokens.append("<\\" + tag + ">")
+        if tag in LINEBREAK_ELEMENTS:
+            self.break_next = True
         self.binary_tokens.append(TAG_TOKEN)
 
 
@@ -67,7 +81,8 @@ class HtmlBodyTextExtractor(HtmlTokenParser):
         text = self._remove_unreadable(text)
         HtmlTokenParser.feed(self, text)
 
-    def _remove_unreadable(self, text):
+    @staticmethod
+    def _remove_unreadable(text):
         """Remove unreadable blocks (head, style, script) from the text.
         This is a necessary pre-processing step because the simple objective function often
         picks long script blocks."""
@@ -137,10 +152,23 @@ class HtmlBodyTextExtractor(HtmlTokenParser):
         return s[0] == '<' and s[-1] == '>'
 
     def body_text(self):
-        """Finds the longest text section which excludes the most tags using an O(N^2) exhaustive search of all
-        eligible text spans."""
+        # memoize result, since it's expensive
         if self.body_txt:
             return self.body_txt
+        (start, end) = self._find_optimal_span()
+
+        text = " ".join(txt for txt in self.tokens[start:end] if not self._is_tag(txt))
+        # get rid of repeated spaces
+        text = MULTI_SPACES_REGEX.sub(' ', text)
+        # strip each line
+        text = '\n'.join([t.strip() for t in text.split('\n')])
+
+        self.body_txt = text
+        return self.body_txt
+
+    def _find_optimal_span(self):
+        """Finds the longest text section which excludes the most tags using an O(N^2) exhaustive search of all
+        eligible text spans."""
         score_max = 0
         i_max = self.body_start_index
         j_max = len(self.encoded) - 1
@@ -157,10 +185,7 @@ class HtmlBodyTextExtractor(HtmlTokenParser):
                     j_max = j
         start = self.total_tokens_before[i_max]
         end = self.total_tokens_before[j_max]
-
-        self.body_txt = " ".join(txt for txt in self.tokens[start:end] if not self._is_tag(txt))
-
-        return self.body_txt
+        return start, end
 
     def full_text(self):
         ft = " ".join(txt for txt in self.tokens if not self._is_tag(txt))
@@ -168,19 +193,11 @@ class HtmlBodyTextExtractor(HtmlTokenParser):
 
 
 if __name__ == '__main__':
-    # with open(sys.argv[1]) as fp:
-    #     soup = BeautifulSoup(fp, 'lxml')
-    # for unreadable in soup.find_all(['head', 'script', 'style']):
-    #     unreadable.extract()
-    # print(soup.get_text())
-    # html = str(soup)
-    # print(html)
     html = open(sys.argv[1]).read()
     p = HtmlBodyTextExtractor()
     p.feed(html)
     p.close()
-    x = p.body_text()
-    print("\nBody text:\n", p.body_text())
+    print(p.body_text())
 
 # (c) 2001 Aidan Finn
 # Released under the terms of the GNU GPL
